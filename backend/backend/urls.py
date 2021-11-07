2855ec81008ecf814cceb469572ed5ca7ffccf94
ZZHUO <31450284+IvesZ@users.noreply.github.com>
2021-11-07 17:25:20 -0500

    set defute url for backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,6 +24,6 @@ router.register(r'todos', views.TodoView, 'todo')
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/', include(router.urls)),
 ]
